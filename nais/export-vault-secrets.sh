f61ef667102bdb1feda65233d0a4f66df5f76e22
moamini <mo.amini@nav.no>
2020-10-14 14:46:04 +0200

    mounter servicebruker fra vault

--- nais/export-vault-secrets.sh
+++ nais/export-vault-secrets.sh
@@ -1,15 +1,15 @@
 ##!/usr/bin/env bash
-#
-#echo "Sjekker eessi-pensjon-oppgave srvPassord"
-#if test -f /var/run/secrets/nais.io/srveessio/password;
-#then
-#  echo "Setter eessi-pensjon-oppgave srvPassord"
-#    export srvpassword=$(cat /var/run/secrets/nais.io/srveessio/password)
-#fi
-#
-#echo "Sjekker eessi-pensjon-oppgave srvUsername"
-#if test -f /var/run/secrets/nais.io/srveessio/username;
-#then
-#    echo "Setter eessi-pensjon-oppgave srvUsername"
-#    export srvusername=$(cat /var/run/secrets/nais.io/srveessio/username)
-#fi
+
+echo "Sjekker eessi-pensjon-statistikk srvPassord"
+if test -f /var/run/secrets/nais.io/srveessipensjonstats/password;
+then
+  echo "Setter eessi-pensjon-statistikk srvPassord"
+    export srvpassword=$(cat /var/run/secrets/nais.io/srveessipensjonstats/password)
+fi
+
+echo "Sjekker eessi-pensjon-statistikk srvUsername"
+if test -f /var/run/secrets/nais.io/srveessipensjonstats/username;
+then
+    echo "Setter eessi-pensjon-statistikk srvUsername"
+    export srvusername=$(cat /var/run/secrets/nais.io/srveessipensjonstats/username)
+fi
