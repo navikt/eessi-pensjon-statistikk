c0dbf894eb6991f0fa0f3e6e1910f6d49d089634
Daniel Skarpås <dskarpas@gmail.com>
2020-10-15 19:36:12 +0200

    legger inn ut-kø for kafka, med ny StatistikkMelding.kt
    Co-authored-by: Mo Amini <mo.amini@nav.no>
    Co-authored-by: Mariam Pervez <mariam.pervez@nav.no>
    Co-authored-by: Nicolas Nordhagen <nicolas.nordhagen@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
@@ -1,25 +1,24 @@
 package no.nav.eessi.pensjon.statistikk.listener
 
-import no.nav.eessi.pensjon.metrics.MetricsHelper
 import no.nav.eessi.pensjon.statistikk.models.StatistikkMelding
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
 import org.slf4j.MDC
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
+import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
 import java.util.*
 import java.util.concurrent.CountDownLatch
 
 @Component
-class StatistikkListener {
+class StatistikkListener (private val kafkaTemplate: KafkaTemplate<String, String>,
+                          @Value("\${kafka.statistikk.topic}") private val statistikkTopic: String) {
 
     private val logger = LoggerFactory.getLogger(StatistikkListener::class.java)
 
-    private lateinit var consumeOutgoing: MetricsHelper.Metric
-    private lateinit var consumeIncoming: MetricsHelper.Metric
-
-    private val latch = CountDownLatch(1)
+     private val latch = CountDownLatch(1)
 
     fun getLatch() = latch
 
@@ -30,25 +29,28 @@ class StatistikkListener {
             autoStartup = "false")
     fun consume(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         MDC.putCloseable("x_request_id", UUID.randomUUID().toString()).use {
-                logger.info("Innkommet statistikk hendelse i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
-                logger.debug(hendelse)
-                try {
+            logger.info("Innkommet statistikk hendelse i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
+            logger.debug(hendelse)
 
-                    logger.debug("Hendelse : $hendelse")
-                    val melding = StatistikkMelding.fromJson(hendelse)
+            try {
+                logger.debug("Hendelse : $hendelse")
+                val melding = StatistikkMelding.fromJson(hendelse)
 
-                    acknowledgment.acknowledge()
-                    logger.info("Acket statistikk melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
-                } catch (ex: Exception) {
-                    logger.error(
-                            "Noe gikk galt under behandling av statistikk-hendelse:\n $hendelse \n" +
-                                    "${ex.message}",
-                            ex
-                    )
-                    throw RuntimeException(ex.message)
-                }
-                latch.countDown()
-
+                produserKafkaMelding(melding)
+                acknowledgment.acknowledge()
+                logger.info("Acket statistikk melding med offset: ${cr.offset()} i partisjon ${cr.partition()}")
+            } catch (ex: Exception) {
+                logger.error("Noe gikk galt under behandling av statistikk-hendelse:\n $hendelse \n", ex)
+                throw RuntimeException(ex.message)
+            }
+            latch.countDown()
         }
+    }
+
+    private fun produserKafkaMelding(melding: StatistikkMelding) {
+        kafkaTemplate.defaultTopic = statistikkTopic
+
+        logger.info("Produserer melding på kafka: ${kafkaTemplate.defaultTopic}  melding: $melding")
+        kafkaTemplate.send(melding).get()
     }
 }
--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/StatistikkMelding.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/StatistikkMelding.kt
@@ -4,7 +4,10 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 
-class StatistikkMelding(
+import org.springframework.messaging.Message
+import org.springframework.messaging.MessageHeaders
+
+class StatistikkMelding  (
         val hendelseType: HendelseType,
         val rinaid: String,
         val bucType: String,
@@ -12,14 +15,26 @@ class StatistikkMelding(
         val saksNummer: String? = null,
         val vetaksId: String? = null,
         val hendelseVersjon: Int? = null
-) {
+) : Message<String> {
     companion object {
         private val sedMapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
 
         fun fromJson(json: String): StatistikkMelding = sedMapper.readValue(json, StatistikkMelding::class.java)
     }
-}
 
+    override fun getPayload(): String {
+        return sedMapper.writeValueAsString(this)
+    }
+
+    override fun getHeaders(): MessageHeaders {
+        return MessageHeaders(
+                mapOf(
+                    "rinaid" to rinaid,
+                    "buctype" to bucType,
+                    "hendelsestype" to hendelseType)
+        )
+    }
+}
 
 enum class HendelseType {
     OPPRETTBUC,
