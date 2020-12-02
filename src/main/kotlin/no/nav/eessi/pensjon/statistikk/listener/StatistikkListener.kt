9a5aa0241e37004d9857f8d51e98c2380e4a12d9
Daniel Skarpås <dskarpas@gmail.com>
2020-12-02 14:28:36 +0100

    Enderer topics for kafka
    Co-authored-by: Mo Amini <mo.amini@nav.no>

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
@@ -1,6 +1,7 @@
 package no.nav.eessi.pensjon.statistikk.listener
 
 import no.nav.eessi.pensjon.statistikk.json.toJson
+import no.nav.eessi.pensjon.statistikk.models.SedHendelseModel
 import no.nav.eessi.pensjon.statistikk.models.StatistikkMelding
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.slf4j.LoggerFactory
@@ -28,7 +29,7 @@ class StatistikkListener (private val kafkaTemplate: KafkaTemplate<String, Strin
             topics = ["\${kafka.statistikk-inn.topic}"],
             groupId = "\${kafka.statistikk-inn.groupid}",
             autoStartup = "false")
-    fun consume(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+    fun consumeBuc(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
         MDC.putCloseable("x_request_id", UUID.randomUUID().toString()).use {
             logger.info("Innkommet statistikk hendelse i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
             logger.debug(hendelse)
@@ -46,6 +47,44 @@ class StatistikkListener (private val kafkaTemplate: KafkaTemplate<String, Strin
             }
             latch.countDown()
         }
+    }
+
+    @KafkaListener(id = "sedSendtListener",
+            idIsGroup = false,
+            topics = ["\${kafka.statistikk-sed-sendt.topic}"],
+            groupId = "\${kafka.statistikk-sed-sendt.groupid}",
+            autoStartup = "false")
+    fun consumeSedSendt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        MDC.putCloseable("x_request_id", UUID.randomUUID().toString()).use {
+            receiveSed(cr, hendelse)
+        }
+    }
+
+    @KafkaListener(id = "sedMottattListener",
+            idIsGroup = false,
+            topics = ["\${kafka.statistikk-sed-mottatt.topic}"],
+            groupId = "\${kafka.statistikk-sed-mottatt.groupid}",
+            autoStartup = "false")
+    fun consumeSedMottatt(hendelse: String, cr: ConsumerRecord<String, String>, acknowledgment: Acknowledgment) {
+        MDC.putCloseable("x_request_id", UUID.randomUUID().toString()).use {
+            receiveSed(cr, hendelse)
+        }
+    }
+
+    private fun receiveSed(cr: ConsumerRecord<String, String>, hendelse: String) {
+        logger.info("Innkommet statistikk sed-motatt i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
+
+        try {
+            val offset = cr.offset()
+            val sedHendelse = SedHendelseModel.fromJson(hendelse)
+            logger.info("*** Starter behandling av SED ${sedHendelse.sedType} BUCtype: ${sedHendelse.bucType} bucid: ${sedHendelse.rinaSakId} ***")
+            kafkaTemplate.send(statistikkUtTopic, sedHendelse.toJson()).get()
+
+        } catch (ex: Exception) {
+            logger.error("Noe gikk galt under behandling av statistikk-sed-hendelse:\n $hendelse \n", ex)
+            throw RuntimeException(ex.message)
+        }
+        latch.countDown()
     }
 
     private fun produserKafkaMelding(melding: StatistikkMelding) {
---
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedHendelseModel.kt
@@ -0,0 +1,33 @@
+package no.nav.eessi.pensjon.statistikk.models
+
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+
+data class SedHendelseModel (
+        val id: Long? = 0,
+        val sedId: String? = null,
+        val sektorKode: String,
+        val bucType: String? = null,
+        val rinaSakId: String,
+        val avsenderId: String? = null,
+        val avsenderNavn: String? = null,
+        val avsenderLand: String? = null,
+        val mottakerId: String? = null,
+        val mottakerNavn: String? = null,
+        val mottakerLand: String? = null,
+        val rinaDokumentId: String,
+        val rinaDokumentVersjon: String? = null,
+        val sedType: SedType? = null,
+        val navBruker: String? = null
+) {
+    companion object {
+        private val sedMapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
+
+        fun fromJson(json: String): SedHendelseModel = sedMapper.readValue(json, SedHendelseModel::class.java)
+    }
+}
+
+
+
+
