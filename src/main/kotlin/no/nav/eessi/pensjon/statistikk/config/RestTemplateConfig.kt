f6b9cff478d67bec376e6bffd71fdff959d1d021
moamini <mo.amini@nav.no>
2020-12-10 10:39:26 +0100

    Legger til restTemplate config for EUX klienten
    Co-authored-by: DS <dskarpas@gmail.com>

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/config/RestTemplateConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/config/RestTemplateConfig.kt
@@ -1,30 +1,36 @@
 package no.nav.eessi.pensjon.statistikk.config
 
-import io.micrometer.core.instrument.MeterRegistry
+import no.nav.eessi.pensjon.logging.RequestIdHeaderInterceptor
+import no.nav.eessi.pensjon.logging.RequestResponseLoggerInterceptor
 import no.nav.eessi.pensjon.security.sts.STSService
+import no.nav.eessi.pensjon.security.sts.UsernameToOidcInterceptor
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.HttpRequest
-import org.springframework.http.MediaType
-import org.springframework.http.client.*
-import java.util.*
+import org.springframework.http.client.BufferingClientHttpRequestFactory
+import org.springframework.http.client.SimpleClientHttpRequestFactory
+import org.springframework.web.client.DefaultResponseErrorHandler
+import org.springframework.web.client.RestTemplate
 
 @Configuration
-class RestTemplateConfig(private val meterRegistry: MeterRegistry) {
-//
-//    @Value("\${srvusername}")
-//    lateinit var username: String
-//
-//    @Value("\${srvpassword}")
-//    lateinit var password: String
+class RestTemplateConfig(private val securityTokenExchangeService: STSService) {
 
+    @Value("\${EUX_RINA_API_V1_URL}")
+    lateinit var euxUrl: String
 
-
-    class RequestInterceptor : ClientHttpRequestInterceptor {
-        override fun intercept(request: HttpRequest, body: ByteArray, execution: ClientHttpRequestExecution): ClientHttpResponse {
-            request.headers["X-Correlation-ID"] = UUID.randomUUID().toString()
-            request.headers["Content-Type"] = MediaType.APPLICATION_JSON.toString()
-            return execution.execute(request, body)
-        }
+    @Bean
+    fun euxOidcRestTemplate(templateBuilder: RestTemplateBuilder): RestTemplate {
+        return templateBuilder
+            .rootUri(euxUrl)
+            .errorHandler(DefaultResponseErrorHandler())
+            .additionalInterceptors(
+                RequestIdHeaderInterceptor(),
+                RequestResponseLoggerInterceptor(),
+                UsernameToOidcInterceptor(securityTokenExchangeService)
+            )
+            .build().apply {
+                requestFactory = BufferingClientHttpRequestFactory(SimpleClientHttpRequestFactory())
+            }
     }
 }
