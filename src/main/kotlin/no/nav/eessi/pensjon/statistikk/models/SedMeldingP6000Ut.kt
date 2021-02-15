cd1eae654a4b3249579d6a656acc6ddc51bea300
moamini <mo.amini@nav.no>
2021-02-15 14:10:31 +0100

    Legger til tostring for SedMelding og SedMeldingP6000
    Co-authored-by: DS <dskarpas@gmail.com>

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingP6000Ut.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingP6000Ut.kt
@@ -7,16 +7,16 @@ import no.nav.eessi.pensjon.eux.BucType
 import no.nav.eessi.pensjon.eux.SedType
 
 class SedMeldingP6000Ut(dokumentId: String,
-                        bucType: BucType,
-                        rinaid: String,
-                        mottakerLand: List<String>? = null,
-                        rinaDokumentVersjon: String,
-                        sedType: SedType,
-                        pid: String? = null,
-                        hendelseType: HendelseType,
-                        pesysSakId: String? = null,
-                        opprettetTidspunkt: String,
-                        vedtaksId: String? = null,
+                        override val bucType: BucType,
+                        override val rinaid: String,
+                        override val mottakerLand: List<String>? = null,
+                        override val rinaDokumentVersjon: String,
+                        override val sedType: SedType,
+                        override val pid: String? = null,
+                        override val hendelseType: HendelseType,
+                        override val pesysSakId: String? = null,
+                        override val opprettetTidspunkt: String,
+                        override val vedtaksId: String? = null,
                         val bostedsland: String? = null,
                         val pensjonsType: String? = null,
                         val vedtakStatus: String? = null,
@@ -41,5 +41,9 @@ class SedMeldingP6000Ut(dokumentId: String,
     companion object {
         private val sedMapper: ObjectMapper = jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
         fun fromJson(json: String): SedMeldingP6000Ut = sedMapper.readValue(json, SedMeldingP6000Ut::class.java)
+    }
+
+    override fun toString(): String {
+        return "SedMeldingP6000Ut(bucType=$bucType, rinaid='$rinaid', mottakerLand=$mottakerLand, rinaDokumentVersjon='$rinaDokumentVersjon', sedType=$sedType, pid=$pid, hendelseType=$hendelseType, pesysSakId=$pesysSakId, opprettetTidspunkt='$opprettetTidspunkt', vedtaksId=$vedtaksId, bostedsland=$bostedsland, pensjonsType=$pensjonsType, vedtakStatus=$vedtakStatus, bruttoBelop=$bruttoBelop, nettoBelop=$nettoBelop, valuta=$valuta, anmodningOmRevurdering=$anmodningOmRevurdering)"
     }
 }
--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingUt.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingUt.kt
@@ -10,21 +10,27 @@ import no.nav.eessi.pensjon.eux.SedType
 @JsonIgnoreProperties(ignoreUnknown = true)
 open class SedMeldingUt (
     val dokumentId: String,
-    val bucType: BucType,
-    val rinaid: String,
-    val mottakerLand: List<String>? = null,
-    val rinaDokumentVersjon: String,
-    val sedType: SedType,
-    val pid: String? = null,
-    val hendelseType: HendelseType,
-    val pesysSakId: String? = null,
-    val opprettetTidspunkt: String,
-    val vedtaksId: String? = null){
+    open val bucType: BucType,
+    open val rinaid: String,
+    open val mottakerLand: List<String>? = null,
+    open val rinaDokumentVersjon: String,
+    open val sedType: SedType,
+    open val pid: String? = null,
+    open val hendelseType: HendelseType,
+    open val pesysSakId: String? = null,
+    open val opprettetTidspunkt: String,
+    open val vedtaksId: String? = null){
 
     companion object {
         private val sedMapper: ObjectMapper =
             jacksonObjectMapper().configure(DeserializationFeature.READ_UNKNOWN_ENUM_VALUES_AS_NULL, true)
         fun fromJson(json: String): SedMeldingUt = sedMapper.readValue(json, SedMeldingUt::class.java)
     }
+
+    override fun toString(): String {
+        return "SedMeldingUt(dokumentId='$dokumentId', bucType=$bucType, rinaid='$rinaid', mottakerLand=$mottakerLand, rinaDokumentVersjon='$rinaDokumentVersjon', sedType=$sedType, pid=$pid, hendelseType=$hendelseType, pesysSakId=$pesysSakId, opprettetTidspunkt='$opprettetTidspunkt', vedtaksId=$vedtaksId)"
+    }
+
+
 }
 
