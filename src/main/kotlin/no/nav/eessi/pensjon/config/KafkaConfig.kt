524a28d03b8e82165639056f73b83c0e1da65e54
moamini <mo.amini@nav.no>
2021-07-07 15:54:14 +0200

    fikser kafkaConfig

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -2,17 +2,23 @@ package no.nav.eessi.pensjon.config
 
 
 import org.apache.kafka.clients.CommonClientConfigs
+import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.clients.producer.ProducerConfig
 import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringDeserializer
+import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 
 @EnableKafka
@@ -26,8 +32,9 @@ class KafkaConfig(
     @param:Value("\${kafka.security.protocol}") private val securityProtocol: String
 ) {
     @Bean
-    fun consumerFactory(): ConsumerFactory<String, String> {
+    fun producerFactory(): ProducerFactory<String, String> {
         val configMap: MutableMap<String, Any> = HashMap()
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SSL"
         configMap[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keystorePath
         configMap[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = credstorePassword
         configMap[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = credstorePassword
@@ -37,27 +44,36 @@ class KafkaConfig(
         configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
         configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
         configMap[ProducerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-statistikk"
-        configMap[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configMap[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        configMap[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = StringSerializer::class.java
+        configMap[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = JsonSerializer::class.java
         configMap[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
+        return DefaultKafkaProducerFactory(configMap)
+    }
+
+    @Bean
+    fun consumerFactory(): ConsumerFactory<String, String> {
+        val configMap: MutableMap<String, Any> = HashMap()
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = "SSL"
+        configMap[SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG] = keystorePath
+        configMap[SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_KEY_PASSWORD_CONFIG] = credstorePassword
+        configMap[SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG] = "JKS"
+        configMap[SslConfigs.SSL_KEYSTORE_TYPE_CONFIG] = "PKCS12"
+        configMap[SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG] = truststorePath
+        configMap[CommonClientConfigs.SECURITY_PROTOCOL_CONFIG] = securityProtocol
+        configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-statistikk"
+        configMap[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
+        configMap[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
+        configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = bootstrapServers
         return DefaultKafkaConsumerFactory(configMap)
     }
 
-/*    @Bean
-    fun sedSendtAuthRetry(registry: KafkaListenerEndpointRegistry): ApplicationRunner? {
-        return ApplicationRunner {
-            val statisikkListener = registry.getListenerContainer("statistikkListener")
-            statisikkListener.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(4L)
-            statisikkListener.start()
-
-            val sedSendtListener = registry.getListenerContainer("sedSendtListener")
-            sedSendtListener.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(4L)
-            sedSendtListener.start()
-
-            val sedMottattListener = registry.getListenerContainer("sedMottattListener")
-            sedMottattListener.containerProperties.authorizationExceptionRetryInterval = Duration.ofSeconds(4L)
-            sedMottattListener.start()
-        }
-    }*/
+    @Bean
+    fun kafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String>? {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = consumerFactory()
+        return factory
+    }
 
 }
