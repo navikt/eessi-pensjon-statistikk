c1c66db98d3d9b4befd93a74ed0294a8fbc5d0b2
Daniel Skarpås <dskarpas@gmail.com>
2021-07-08 12:17:39 +0200

    Tester litt på kafka

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -8,12 +8,18 @@ import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.StringSerializer
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.ApplicationRunner
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
-import org.springframework.kafka.core.*
+import org.springframework.kafka.config.KafkaListenerEndpointRegistry
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.serializer.JsonDeserializer
 import org.springframework.kafka.support.serializer.JsonSerializer
 
@@ -78,4 +84,10 @@ class KafkaConfig(
         return factory
     }
 
+    @Bean
+    fun sedSendtAuthRetry(registry: KafkaListenerEndpointRegistry): ApplicationRunner? {
+        return ApplicationRunner {
+            registry.getListenerContainer("statistikkListener").start()
+        }
+    }
 }
