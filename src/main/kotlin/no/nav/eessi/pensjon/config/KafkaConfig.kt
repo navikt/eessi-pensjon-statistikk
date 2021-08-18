f536452199958d54292dce951e424e7c831b4eb4
moamini <mo.amini@nav.no>
2021-08-18 14:25:27 +0200

    prøver removeTypeHeaders

--- src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/config/KafkaConfig.kt
@@ -55,15 +55,20 @@ class KafkaConfig(
 
     @Bean
     fun aivenKafkaConsumerFactory(): ConsumerFactory<String, String> {
+        val keyDeserializer: JsonDeserializer<String> = JsonDeserializer(String::class.java)
+        keyDeserializer.setRemoveTypeHeaders(true)
+   /*     deserializer.addTrustedPackages("*")
+        deserializer.setUseTypeMapperForKey(true)*/
+
+        val valueDeserializer = StringDeserializer()
+
         val configMap: MutableMap<String, Any> = HashMap()
         populerAivenCommonConfig(configMap)
         configMap[ConsumerConfig.CLIENT_ID_CONFIG] = "eessi-pensjon-statistikk"
-        configMap[ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG] = StringDeserializer::class.java
-        configMap[ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG] = JsonDeserializer::class.java
         configMap[ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG] = aivenBootstrapServers
         configMap[ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG] = false
 
-        return DefaultKafkaConsumerFactory(configMap)
+        return DefaultKafkaConsumerFactory(configMap, keyDeserializer, valueDeserializer)
     }
 
     @Bean
