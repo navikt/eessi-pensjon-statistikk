b899192644e0403e4426191fae1e3f0fe6c79c66
Mo Amini <mo.amini@nav.no>
2022-02-10 12:43:02 +0100

    Hopper over SEDer som vi ikke har ansvar for å journalføre da det er vanskelig å vite om det omhandler Pensjon i horisontale og administrative SEDer

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/GyldigeHendelser.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/GyldigeHendelser.kt
@@ -1,5 +1,7 @@
 package no.nav.eessi.pensjon.statistikk.listener
 
+import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.eux.model.SedType.*
 import no.nav.eessi.pensjon.eux.model.buc.BucType
 
 class GyldigeHendelser {
@@ -9,15 +11,25 @@ class GyldigeHendelser {
         private val gyldigeInnkommendeBucTyper = listOf(BucType.H_BUC_07, BucType.R_BUC_02)
         private val gyldigUtgaaendeBucType = BucType.R_BUC_02
 
+        /**
+         * SED-typer vi IKKE behandler i Journalføring.
+         */
+        val ugyldigeTyper: Set<SedType> = setOf(
+            P13000, X001, X002, X003, X004, X006, X007, X009,
+            X011, X012, X013, X050, X100, H001, H002, H020, H021, H120, H121, R004, R006
+        )
+
         fun mottatt(hendelse: SedHendelseRina) =
                 when {
-                    hendelse.bucType in gyldigeInnkommendeBucTyper || gyldigSektorKode == hendelse.sektorKode -> true
+                    (hendelse.bucType in gyldigeInnkommendeBucTyper || gyldigSektorKode == hendelse.sektorKode)
+                            || hendelse.sedType !in ugyldigeTyper -> true
                     else -> false
                 }
 
         fun sendt(hendelse: SedHendelseRina) =
                 when {
-                    gyldigUtgaaendeBucType == hendelse.bucType || gyldigSektorKode == hendelse.sektorKode -> true
+                    (gyldigUtgaaendeBucType == hendelse.bucType || gyldigSektorKode == hendelse.sektorKode)
+                            || hendelse.sedType !in ugyldigeTyper -> true
                     else -> false
                 }
     }
