7894209d06916b6f1cbca29db0a2915e8dc58921
Daniel Skarpås <dskarpas@gmail.com>
2022-03-14 14:10:02 +0100

    skipper offset: 10555L i consumeOpprettelseMelding

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
@@ -63,7 +63,7 @@ class StatistikkListener(
             logger.info("Innkommet statistikk hendelse i partisjon: ${cr.partition()}, med offset: ${cr.offset()}")
 
             opprettMeldingMetric.measure {
-                val offsetToSkip = listOf(10539L, 10514L, 10546L, 10550L)
+                val offsetToSkip = listOf(10514L, 10539L, 10546L, 10550L, 10555L)
                 try {
                     val offset = cr.offset()
                     if (offsetToSkip.contains(offset)) {
