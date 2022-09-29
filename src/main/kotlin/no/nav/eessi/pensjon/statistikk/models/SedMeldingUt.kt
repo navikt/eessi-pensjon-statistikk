80d07d1eb64fbfd8ed76bd4a9a10ac6587d74ccd
Kjetil JD <kjetil.jorgensen-dahl@nav.no>
2022-09-29 16:47:04 +0200

    B plukker nå norsk saksnummer fra SED til meldinger
    Tidligere ble potensielt utenlands saksnummer puttet i pesysSakId.
    Vi har fortsatt ingen garanti for at det er en Pesys-sakid, men oddsen er større.

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingUt.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/models/SedMeldingUt.kt
@@ -6,6 +6,7 @@ import no.nav.eessi.pensjon.eux.model.buc.BucType
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 open class SedMeldingUt (
+    /* Om felt endres - husk å regenerer equals/hashcode/toString */
     open val dokumentId: String,
     open val bucType: BucType,
     open val rinaid: String, // rinaSakId
@@ -19,8 +20,49 @@ open class SedMeldingUt (
     open val opprettetTidspunkt: String,
     open val vedtaksId: String? = null){
 
+    /* Genereres f eks med IDEA. */
     override fun toString(): String {
         return "SedMeldingUt(dokumentId='$dokumentId', bucType=$bucType, rinaid='$rinaid', mottakerLand=$mottakerLand, rinaDokumentVersjon='$rinaDokumentVersjon', sedType=$sedType, pid=$pid, hendelseType=$hendelseType, pesysSakId=$pesysSakId, opprettetTidspunkt='$opprettetTidspunkt', vedtaksId=$vedtaksId)"
+    }
+
+    /* Genereres f eks med IDEA. */
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as SedMeldingUt
+
+        if (dokumentId != other.dokumentId) return false
+        if (bucType != other.bucType) return false
+        if (rinaid != other.rinaid) return false
+        if (mottakerLand != other.mottakerLand) return false
+        if (avsenderLand != other.avsenderLand) return false
+        if (rinaDokumentVersjon != other.rinaDokumentVersjon) return false
+        if (sedType != other.sedType) return false
+        if (pid != other.pid) return false
+        if (hendelseType != other.hendelseType) return false
+        if (pesysSakId != other.pesysSakId) return false
+        if (opprettetTidspunkt != other.opprettetTidspunkt) return false
+        if (vedtaksId != other.vedtaksId) return false
+
+        return true
+    }
+
+    /* Genereres f eks med IDEA. */
+    override fun hashCode(): Int {
+        var result = dokumentId.hashCode()
+        result = 31 * result + bucType.hashCode()
+        result = 31 * result + rinaid.hashCode()
+        result = 31 * result + mottakerLand.hashCode()
+        result = 31 * result + (avsenderLand?.hashCode() ?: 0)
+        result = 31 * result + rinaDokumentVersjon.hashCode()
+        result = 31 * result + sedType.hashCode()
+        result = 31 * result + (pid?.hashCode() ?: 0)
+        result = 31 * result + hendelseType.hashCode()
+        result = 31 * result + (pesysSakId?.hashCode() ?: 0)
+        result = 31 * result + opprettetTidspunkt.hashCode()
+        result = 31 * result + (vedtaksId?.hashCode() ?: 0)
+        return result
     }
 }
 
--- src/test/kotlin/no/nav/eessi/pensjon/statistikk/services/HendelsesAggregeringsServiceTest.kt
+++ src/test/kotlin/no/nav/eessi/pensjon/statistikk/services/HendelsesAggregeringsServiceTest.kt
@@ -6,11 +6,14 @@ import no.nav.eessi.pensjon.ResourceHelper
 import no.nav.eessi.pensjon.eux.BucMetadata
 import no.nav.eessi.pensjon.eux.EuxService
 import no.nav.eessi.pensjon.eux.Sed
+import no.nav.eessi.pensjon.eux.model.SedType
+import no.nav.eessi.pensjon.eux.model.buc.BucType
 import no.nav.eessi.pensjon.gcp.GcpStorageService
 import no.nav.eessi.pensjon.json.toJson
 import no.nav.eessi.pensjon.statistikk.models.HendelseType
 import no.nav.eessi.pensjon.statistikk.models.PensjonsType
 import no.nav.eessi.pensjon.statistikk.models.SedMeldingP6000Ut
+import no.nav.eessi.pensjon.statistikk.models.SedMeldingUt
 import no.nav.eessi.pensjon.statistikk.models.VedtakStatus
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -25,7 +28,7 @@ internal class HendelsesAggregeringsServiceTest {
     fun `gitt en sedOpprettet melding så populer sedOpprettetMeldingUt`() {
         val pesysSaksID = "22929783"
         val dokumentId = "d740047e730f475aa34ae59f62e3bb99"
-        val rinaid = "111"
+        val rinaSakId = "111"
         val vedtaksId = "333"
         val mottakerland = listOf("NO")
 
@@ -35,23 +38,29 @@ internal class HendelsesAggregeringsServiceTest {
         every { euxService.getBucMetadata(any())} returns buc
 
         // Mocker SED
-        val p6000Json = ResourceHelper.getResourceSed("sed/P2000-preutfylt-fnr-og-sakid.json").toJson()
-        val p6000 = Sed.fromJson(p6000Json)
-        every { euxService.getSed(any(), any()) } returns p6000
+        val jsonSed = ResourceHelper.getResourceSed("sed/P2000-preutfylt-fnr-og-sakid.json").toJson()
+        val sed = Sed.fromJson(jsonSed)
+        every { euxService.getSed(any(), any()) } returns sed
 
-        val sedOpprettetMeldingUt = infoService.aggregateSedOpprettetData(rinaid, dokumentId, vedtaksId)
+        val sedOpprettetMeldingUt = infoService.aggregateSedOpprettetData(rinaSakId, dokumentId, vedtaksId)
 
-        assertEquals(sedOpprettetMeldingUt?.rinaid, rinaid)
-        assertEquals(sedOpprettetMeldingUt?.dokumentId, dokumentId)
-        assertEquals(sedOpprettetMeldingUt?.pesysSakId, pesysSaksID)
-        assertEquals(sedOpprettetMeldingUt?.opprettetTidspunkt, "2020-12-08T09:53:36.241Z")
-        assertEquals(sedOpprettetMeldingUt?.vedtaksId, vedtaksId)
-        assertEquals(sedOpprettetMeldingUt?.mottakerLand, mottakerland)
-        assertEquals(sedOpprettetMeldingUt?.rinaDokumentVersjon, "2")
+        assertEquals(SedMeldingUt(
+            dokumentId = dokumentId,
+            bucType = BucType.P_BUC_01,
+            rinaid = rinaSakId,
+            mottakerLand = mottakerland,
+            avsenderLand = null, // finnes ikke for opprettetmelding (er vel alltid Norge)
+            rinaDokumentVersjon = "2",
+            sedType = SedType.P2000,
+            pid = "08035325493",
+            hendelseType = HendelseType.SED_OPPRETTET,
+            pesysSakId = pesysSaksID,
+            opprettetTidspunkt = "2020-12-08T09:53:36.241Z",
+            vedtaksId = vedtaksId), sedOpprettetMeldingUt)
     }
 
     @Test
-    fun `gitt en sedOpprettet for P6000 melding så populer sedOpprettetMeldingUt`() {
+    fun `gitt en sedOpprettet for P6000 melding så populer SedMeldingUt`() {
         //Mocker BUC
         val bucJson = ResourceHelper.getResourceBucMetadata("buc/bucMedP6000.json").toJson()
         val buc = BucMetadata.fromJson(bucJson)
@@ -63,23 +72,29 @@ internal class HendelsesAggregeringsServiceTest {
         every { euxService.getSed(any(), any()) } returns p6000
 
         val dokumentId = "08e5310500a94640abfb309e481ca319"
-        val rinaId = "1271728"
+        val rinaSakId = "1271728"
         val mottakerland = listOf("NO")
 
-        val sedOpprettetMeldingUt = infoService.aggregateSedOpprettetData(rinaId, dokumentId, null)
+        val sedOpprettetMeldingUt = infoService.aggregateSedOpprettetData(rinaSakId, dokumentId, null)
 
-        assertEquals(sedOpprettetMeldingUt?.rinaid, rinaId)
-        assertEquals(sedOpprettetMeldingUt?.dokumentId, dokumentId)
-        assertEquals(sedOpprettetMeldingUt?.pesysSakId, "22919968")
-        assertEquals(sedOpprettetMeldingUt?.opprettetTidspunkt, "2021-02-11T13:08:29.914Z")
-        assertEquals(sedOpprettetMeldingUt?.vedtaksId, null)
-        assertEquals(sedOpprettetMeldingUt?.mottakerLand, mottakerland)
-        assertEquals(sedOpprettetMeldingUt?.rinaDokumentVersjon, "5")
-
+        assertEquals(SedMeldingUt(
+            dokumentId = dokumentId,
+            bucType = BucType.P_BUC_06,
+            rinaid = rinaSakId,
+            mottakerLand = mottakerland,
+            avsenderLand = null, // finnes ikke for opprettetmelding (er vel alltid Norge)
+            rinaDokumentVersjon = "5",
+            sedType = SedType.P6000,
+            pid = "09028020144",
+            hendelseType = HendelseType.SED_OPPRETTET,
+            pesysSakId = "22919968",
+            opprettetTidspunkt = "2021-02-11T13:08:29.914Z",
+            vedtaksId = null
+        ), sedOpprettetMeldingUt)
     }
 
     @Test
-    fun `gitt en sed sendt for P6000 melding så populer SedMeldingUt `() {
+    fun `gitt en sed sendt for P6000 melding så populer SedMeldingP6000Ut `() {
         //Mocker BUC
         val bucJson = ResourceHelper.getResourceBucMetadata("buc/bucMedP6000.json").toJson()
         val buc = BucMetadata.fromJson(bucJson)
@@ -91,25 +106,29 @@ internal class HendelsesAggregeringsServiceTest {
         every { euxService.getSed(any(), any()) } returns p6000
 
         val dokumentId = "08e5310500a94640abfb309e481ca319"
-        val rinaId = "1271728"
+        val rinaSakId = "1271728"
         val mottakerland = listOf("NO")
 
-        val sedOpprettetMeldingUt = infoService.populerSedMeldingUt(rinaId, dokumentId, null, HendelseType.SED_SENDT, "SE") as SedMeldingP6000Ut
+        val sedOpprettetMeldingUt = infoService.populerSedMeldingUt(rinaSakId, dokumentId, null, HendelseType.SED_SENDT, "SE") as SedMeldingP6000Ut
 
-        assertEquals(sedOpprettetMeldingUt.rinaid, rinaId)
-        assertEquals(sedOpprettetMeldingUt.dokumentId, dokumentId)
-        assertEquals(sedOpprettetMeldingUt.pesysSakId, "22919968")
-        assertEquals(sedOpprettetMeldingUt.opprettetTidspunkt, "2021-02-11T13:08:29.914Z")
-        assertEquals(sedOpprettetMeldingUt.vedtaksId, null)
-        assertEquals(sedOpprettetMeldingUt.mottakerLand, mottakerland)
-        assertEquals(sedOpprettetMeldingUt.rinaDokumentVersjon, "5")
-
-        //SedMeldingP6000Ut
-        assertEquals(sedOpprettetMeldingUt.bostedsland, "HR")
-        assertEquals(sedOpprettetMeldingUt.bruttoBelop, "12482")
-        assertEquals(sedOpprettetMeldingUt.valuta, "NOK")
-        assertEquals(sedOpprettetMeldingUt.pensjonsType, PensjonsType.GJENLEV)
-        assertEquals(sedOpprettetMeldingUt.vedtakStatus, VedtakStatus.FORELOPIG_UTBETALING)
-        assertEquals(sedOpprettetMeldingUt.avsenderLand, "SE")
+        assertEquals(SedMeldingP6000Ut(
+            dokumentId = dokumentId,
+            bucType = BucType.P_BUC_06,
+            rinaid = rinaSakId,
+            mottakerLand = mottakerland,
+            avsenderLand = "SE",
+            rinaDokumentVersjon = "5",
+            sedType = SedType.P6000,
+            pid = "09028020144",
+            hendelseType = HendelseType.SED_SENDT,
+            pesysSakId = "22919968",
+            opprettetTidspunkt = "2021-02-11T13:08:29.914Z",
+            vedtaksId = null,
+            bostedsland = "HR",
+            pensjonsType = PensjonsType.GJENLEV,
+            vedtakStatus = VedtakStatus.FORELOPIG_UTBETALING,
+            bruttoBelop = "12482",
+            valuta = "NOK"
+        ), sedOpprettetMeldingUt)
     }
 }
