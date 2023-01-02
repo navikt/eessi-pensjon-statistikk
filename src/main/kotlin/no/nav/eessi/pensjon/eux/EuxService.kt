941e19e6b9fe6e06557f29ed8812ea37f5e9c71e
Daniel Skarpås <dskarpas@gmail.com>
2023-01-02 15:21:44 +0100

    f - Retryable for hentBuc og hentSed

--- src/main/kotlin/no/nav/eessi/pensjon/eux/EuxService.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/eux/EuxService.kt
@@ -3,6 +3,13 @@ package no.nav.eessi.pensjon.eux
 import no.nav.eessi.pensjon.eux.klient.EuxKlientLib
 import no.nav.eessi.pensjon.utils.mapJsonToAny
 import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Profile
+import org.springframework.retry.RetryCallback
+import org.springframework.retry.RetryContext
+import org.springframework.retry.annotation.Backoff
+import org.springframework.retry.annotation.Retryable
+import org.springframework.retry.listener.RetryListenerSupport
+import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 
 
@@ -12,13 +19,16 @@ class EuxService(private val euxKlient: EuxKlientLib){
 
     private val logger = LoggerFactory.getLogger(EuxService::class.java)
 
+    @Retryable(
+        backoff = Backoff(delayExpression = "@euxKlientRetryConfig.initialRetryMillis", maxDelay = 200000L, multiplier = 3.0),
+        listeners  = ["euxKlientBucRetryLogger"]
+    )
     fun getBucMetadata(rinaSakId: String) : BucMetadata? {
         val metaData = euxKlient.hentBucJson(rinaSakId = rinaSakId)
         logger.debug("bucmetadata: ${metaData}")
 
         return metaData?.let { mapJsonToAny(it) }
     }
-
 
     /**
      * Henter norsk sakID fra SED
@@ -29,6 +39,10 @@ class EuxService(private val euxKlient: EuxKlientLib){
      * Returnerer null dersom det ikke finnes noen norske sakIder i listen
      *
      */
+    @Retryable(
+        backoff = Backoff(delayExpression = "@euxKlientRetryConfig.initialRetryMillis", maxDelay = 200000L, multiplier = 3.0),
+        listeners  = ["euxKlientRetryLogger"]
+    )
     fun getSed(rinaSakId: String, dokumentId : String) : Sed? {
         val sedAsJson = euxKlient.hentSedJson(rinaSakId, dokumentId)
 
@@ -36,3 +50,16 @@ class EuxService(private val euxKlient: EuxKlientLib){
         return sedAsJson?.let { mapJsonToAny(sedAsJson)}
     }
 }
+
+@Profile("!retryConfigOverride")
+@Component
+data class EuxKlientRetryConfig(val initialRetryMillis: Long = 20000L)
+
+@Component
+class EuxKlientRetryLogger : RetryListenerSupport() {
+    private val logger = LoggerFactory.getLogger(EuxKlientRetryLogger::class.java)
+    override fun <T : Any?, E : Throwable?> onError(context: RetryContext?, callback: RetryCallback<T, E>?, throwable: Throwable?) {
+        logger.warn("Feil under henting fra EUX - try #${context?.retryCount } - ${throwable?.toString()}", throwable)
+    }
+}
+
