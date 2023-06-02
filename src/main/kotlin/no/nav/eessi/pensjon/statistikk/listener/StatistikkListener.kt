c76192ce12da867acb6e2d0f17cec30fbe4dd73a
Daniel Skarpås <dskarpas@gmail.com>
2023-06-02 09:49:20 +0200

    t - Ignorerer offset 55362L; feil i rina

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
@@ -70,7 +70,7 @@ class StatistikkListener(
                 try {
                     val melding = meldingsMapping(hendelse)
                     logger.info("Oppretter melding av type: $hendelse")
-                    val hopperOverOffset = listOf(43313L, 43318L, 43461L, 43631L, 43499L)
+                    val hopperOverOffset = listOf(43313L, 43318L, 43461L, 43631L, 43499L, 55362L)
                     if (MissingBuc.checkForMissingBuc(melding.rinaId) || cr.offset() in hopperOverOffset) {
                         logger.warn("Hopper over offset: ${cr.offset()}")
                     } else {
