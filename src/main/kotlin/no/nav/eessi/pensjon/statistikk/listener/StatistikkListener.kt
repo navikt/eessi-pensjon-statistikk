15c316c21bb50bf3a45be8a78ad16e2163f6ea6e
Daniel Skarpås <dskarpas@gmail.com>
2025-12-09 14:36:28 +0100

    B - Skipper offset 157561; feil med buc 12029466

--- src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
+++ src/main/kotlin/no/nav/eessi/pensjon/statistikk/listener/StatistikkListener.kt
@@ -65,7 +65,7 @@ class StatistikkListener(
                     val melding = meldingsMapping(hendelse)
                     logger.info("Oppretter melding av type: $hendelse")
 
-                    val hopperOverOffset = listOf<Long>(157817, 157529, 157838)
+                    val hopperOverOffset = listOf<Long>(157817, 157529, 157838, 157561)
                     if (MissingBuc.checkForMissingBuc(melding.rinaId) || cr.offset() in hopperOverOffset) {
                         logger.warn("Hopper over offset: ${cr.offset()}")
                     } else {
